a49693582d7d20068ed3afb222b8c3adae22c8d2
Peter Chien <peter2380123@gmail.com>
2019-09-10 03:13:18 +1000

    Squashed commit of the following:
    commit 8040b209d63452c59c680d851d86916695f1f071
    Author: Peter Chien <peter2380123@gmail.com>
    Date:   Tue Sep 10 03:04:09 2019 +1000
    
        Removed right-panel, now using pagination
        ... Looks better now. Detaiedl changes include:
        1. Pagination docked using google's Controls thingy.
        2. Button to clear all stops also docked, and docked under stop list.
        3. CSS beautification of above items.
        4. Fixed add-stop limit checker (was triggering one-stop late)
        5. Due to ordered list not doing its job in pagination, MANUAL stop
        alphebet ordering is done with String edits. List is purely for user
        view though, system should be happy with it.
    
    commit b8e36589b2c5c20b2c58185352f58939f77a4b82
    Author: Peter Chien <peter2380123@gmail.com>
    Date:   Tue Sep 10 01:23:16 2019 +1000
    
        Added clear-all-stops button and re-routes
    
        1. Button relocated to on top of the map.
        2. Button CSS set.
        3. Button now also clears Stopover side panel (white).
        4. Added sanity check to prevent adding stopover before origin and
        destination are set.
    
    commit d5ff5834c0e99ee9d8401e33e6e9ac05063d64c7
    Author: Peter Chien <peter2380123@gmail.com>
    Date:   Mon Sep 9 23:29:22 2019 +1000
    
        Added multi-waypoints, stopover list update
        Other minor changes:
        1. Removed types of travel option. Limiting to driving only as walking
        long distance trip is impractical.
        2. Added a shortcut icon <link> in index.html to workaround error.
    
    commit 4865178911aa28770e2d988a74d6874cec01f79e
    Author: Peter Chien <peter2380123@gmail.com>
    Date:   Mon Sep 9 20:33:24 2019 +1000
    
        Removed travel mode selection, deemed unhelpful
        Contains additional patches:
        1. Changed file location of styles.css to a stylesheets subfolder.
        2. Pre-allocated a right-panel for future uses. Okay to remove if not
        needed.

--- public/src/map_func.js
+++ public/src/map_func.js
@@ -8,11 +8,14 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     mapTypeControl: false,
     center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13
+    zoom: 13,
+    streetViewControl: false
   });
 
   new AutocompleteDirectionsHandler(map);
 }
+
+var waypts = [];
 
 /**
  * @constructor
@@ -21,48 +24,52 @@ function AutocompleteDirectionsHandler(map) {
   this.map = map;
   this.originPlaceId = null;
   this.destinationPlaceId = null;
-  this.travelMode = 'WALKING';
+  this.travelMode = 'DRIVING';
   this.directionsService = new google.maps.DirectionsService;
   this.directionsRenderer = new google.maps.DirectionsRenderer;
   this.directionsRenderer.setMap(map);
 
   var originInput = document.getElementById('origin-input');
   var destinationInput = document.getElementById('destination-input');
-  var modeSelector = document.getElementById('mode-selector');
+  var stopoverInput = document.getElementById('stopover-input');
+  var clearStops = document.getElementById('clear-all-button');
+  var stopList = document.getElementById('right-panel');
 
   var originAutocomplete = new google.maps.places.Autocomplete(originInput);
   // Specify just the place data fields that you need.
   originAutocomplete.setFields(['place_id']);
 
-  var destinationAutocomplete =
-      new google.maps.places.Autocomplete(destinationInput);
+  var destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
   // Specify just the place data fields that you need.
   destinationAutocomplete.setFields(['place_id']);
 
-  this.setupClickListener('changemode-walking', 'WALKING');
-  this.setupClickListener('changemode-transit', 'TRANSIT');
-  this.setupClickListener('changemode-driving', 'DRIVING');
+  var stopoverAutocomplete = new google.maps.places.Autocomplete(stopoverInput);
+  stopoverAutocomplete.setFields(['place_id']);
+
+  // Setup click event listener at clear button
+  this.setupClickListener('clear-all-button')
 
   this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
   this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+  this.setupPlaceChangedListener(stopoverAutocomplete, 'STOP');
 
   this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
   this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(stopoverInput);
+  this.map.controls[google.maps.ControlPosition.RIGHT_TOP].push(stopList);
+  this.map.controls[google.maps.ControlPosition.RIGHT_TOP].push(clearStops);
 }
 
-// Sets a listener on a radio button to change the filter type on Places
-// Autocomplete.
-AutocompleteDirectionsHandler.prototype.setupClickListener = function(
-    id, mode) {
-  var radioButton = document.getElementById(id);
-  var me = this;
-
-  radioButton.addEventListener('click', function() {
-    me.travelMode = mode;
-    me.route();
-  });
-};
+// Handles event upon clear button being clicked
+AutocompleteDirectionsHandler.prototype.setupClickListener = function(id) {
+    var clearAllButton = document.getElementById(id);
+    var me = this;
+    clearAllButton.addEventListener('click', function(){
+      waypts = [];
+      document.getElementById('stops').innerHTML = "";
+      me.route();
+    });
+  };
 
 AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
     autocomplete, mode) {
@@ -78,24 +85,59 @@ AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(
     }
     if (mode === 'ORIG') {
       me.originPlaceId = place.place_id;
-    } else {
+    } else if (mode === 'DEST') {
       me.destinationPlaceId = place.place_id;
+    } else if (mode === 'STOP') { // this should keep track to stopovers
+      if(!me.destinationPlaceId || !me.destinationPlaceId){
+        alert("Please set origin and destination first.")
+        document.getElementById('stopover-input').value = ""; // clears input textbox
+        return
+      }
+      if(waypts.length >= 8){
+        alert("You have reached the limit of 10 total waypoints!");
+        document.getElementById('stopover-input').value = ""; // clears input textbox
+        return
+      }
+      me.stopoverPlaceId = place.place_id;
+      addStopover(place.place_id);
     }
     me.route();
   });
 };
 
+function addStopover(place_id){
+  var waypointElmts = document.getElementById('stopover-input').value;
+  var ol = document.getElementById('stops');
+  var li = document.createElement('li');
+  var order = String.fromCharCode('B'.charCodeAt()+waypts.length);
+  li.textContent = order+ ". " + waypointElmts;
+  ol.appendChild(li);
+
+  if(waypointElmts.length > 0) // sanity check (?)
+  {
+    waypts.push({
+      location: waypointElmts,
+      stopover: true
+    });
+    document.getElementById('stopover-input').value = ""; // clears input textbox
+  }
+  
+}
+
 AutocompleteDirectionsHandler.prototype.route = function() {
   if (!this.originPlaceId || !this.destinationPlaceId) {
     return;
   }
+  
   var me = this;
 
   this.directionsService.route(
       {
         origin: {'placeId': this.originPlaceId},
         destination: {'placeId': this.destinationPlaceId},
-        travelMode: this.travelMode
+        waypoints: waypts,
+        optimizeWaypoints:false,
+        travelMode: 'DRIVING'
       },
       function(response, status) {
         if (status === 'OK') {
@@ -105,4 +147,5 @@ AutocompleteDirectionsHandler.prototype.route = function() {
         }
       });
 };
+
 //-----------------------------------------------------------------------------------//
